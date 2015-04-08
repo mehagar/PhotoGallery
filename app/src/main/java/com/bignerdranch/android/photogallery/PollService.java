fef6e9cf08dd74bf27ed03c36959c60071e23366
mehagar <michaelehagar@gmail.com>
2015-04-08 17:12:27 -0500

    Started Ch. 29

---
+++ app/src/main/java/com/bignerdranch/android/photogallery/PollService.java
@@ -0,0 +1,26 @@
+package com.bignerdranch.android.photogallery;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.content.Context;
+import android.util.Log;
+
+/**
+ * An {@link IntentService} subclass for handling asynchronous task requests in
+ * a service on a separate handler thread.
+ * <p/>
+ * TODO: Customize class - update intent actions, extra parameters and static
+ * helper methods.
+ */
+public class PollService extends IntentService {
+    private static final String TAG = "PollService";
+
+    public PollService() {
+        super(TAG);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Log.i(TAG, "Received an intent: " + intent);
+    }
+}
